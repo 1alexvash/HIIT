80ddb75f33ce515cdb1cff8e7f7e0e0323320c1a
Alexander <1alexvash@gmail.com>
2019-07-03 16:52:33 +0300

    Updates

---
+++ precache-manifest.fb3d7e506b9cc9c8e43978a994caaf7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41f7ee987b5d6eab63ab3f26a77ea88",
+    "url": "/HIIT/index.html"
+  },
+  {
+    "revision": "5845338a3f0029dbd805",
+    "url": "/HIIT/static/css/main.afb1cb6b.chunk.css"
+  },
+  {
+    "revision": "3fea17f25c9856634d7e",
+    "url": "/HIIT/static/js/2.1bb12975.chunk.js"
+  },
+  {
+    "revision": "5845338a3f0029dbd805",
+    "url": "/HIIT/static/js/main.2d09bd79.chunk.js"
+  },
+  {
+    "revision": "e86d37a32a05734eafb0",
+    "url": "/HIIT/static/js/runtime~main.83aedd31.js"
+  }
+]);
